fb9a591de7ae5679f9e7f7e8d0f3a3c9e6a676b5
Alex  Contreras <alex.code29@gmail.com>
2020-08-20 02:53:00 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,17 @@
 def unsafe?(speed)
-
-end
+  if speed < 40 || speed > 60
+    true
+  else speed == 50
+    false
+  end 
+ end
 
 
 
 def not_safe?(speed)
+  speed < 40 || speed > 60 ? true : false
+
+ 
 	
 end
 	
